233029aa74b2d52bcc579a3e69e4d3e3b224f0d8
brishavK71 <119672530+brishavK71@users.noreply.github.com>
2023-05-22 14:41:39 +0545

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 # Change to your application's directory
 cd /home/ec2-user/server/src
-
 # Install dependencies
 npm install
-
 # Build your ReactJS application
 npm run build
-
 # Start the application using PM2
 pm2 start npm --name "react-app" -- start
-
 # Save the PM2 process list
 pm2 save
-
 # Generate the startup script
 pm2 startup
-
 # Restart PM2 to ensure the application starts on system reboot
 pm2 restart all
