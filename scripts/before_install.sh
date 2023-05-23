b58c9fae691e6f567bfe4c37dcd34c4b62782dea
brishavK71 <119672530+brishavK71@users.noreply.github.com>
2023-05-23 21:54:36 +0545

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-sudo su 
-yum update
-yum install ruby
-cd /home/ec2-user
-wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
-chmod +x ./install
-./install auto
-
 cd /home/ec2-user/server
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 yum -y install nodejs npm
