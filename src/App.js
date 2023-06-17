b615f0b7a690417b573f95fcc7548ead146afa6c
brishavK71 <119672530+brishavK71@users.noreply.github.com>
2023-06-17 16:04:03 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 function App() {
   return (
-<<<<<<< HEAD
-    <>
-    Hello World This is a React App hosted through a pipeline
-    </>
-=======
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+hello world this a react app hosted through a CICD pipeline
       </header>
     </div>
->>>>>>> parent of d734a60 (Update App.js)
+
   );
 }
 
